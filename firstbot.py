49d4656fab949a05254f0211c498d0946f04bf9d
AlphCode <38281563+Patxio69@users.noreply.github.com>
2019-11-19 16:36:53 +0530

    mod_python template : CSDOJO

--- firstbot.py
+++ firstbot.py
@@ -1,11 +1,11 @@
-import 
+import tweepy
 
-cons_key=""
-secret_key=""
-token1=""
-token2=""
+consumer_key=""
+consumer_secret=""
+Access_key="923299188938592256-0ZKg0lg5PNLG7MlIQ9DHID7OgqWfJgr"
+Access_secret="Mxg6TSfrSbx6mFN6x7ZXblzcPgoLYn4L1I9LJHT9UC5oq"
 
-any_varname = #tweet module name.Outhstuff(cons_key, secret_key)
-any_varname.#tokem(token1,token2)
-
+any_varname = tweepy.OAuthHandler(consumer_key, consumer_secret) #tweet module name.Outhstuff(cons_key, secret_key)
+any_varname.get_access_token(Access_key, Access_secret)
+api = tweepy.API(auth)
 
